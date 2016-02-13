871e80b43ef6b4f407c5445a773d0bcd8908cdf2
alanlv200 <vistlv200@gmail.com>
2016-02-13 16:47:04 +0900

    first commit

--- TechMonster/LobbyViewController.swift
+++ TechMonster/LobbyViewController.swift
@@ -7,13 +7,45 @@
 //
 
 import UIKit
+import AVFoundation
 
-class LobbyViewController: UIViewController {
+class LobbyViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    var stamina: Float = 0
+    var staminaTimer: NSTimer!
+    var util: TechDraUtility!
+    var player: Player!
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var staminaBar: UIProgressView!
+    @IBOutlet var levelLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        player = Player()
+        
+        staminaBar.transform = CGAffineTransformMakeScale(1.0, 4.0)
+        
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        var level: Int = userDefaults.integerForKey("level")
+        
+        nameLabel.text = player.name
+        levelLabel.text = String(format: "Lv %d", level + 1)
+        stamina = 100
+        
+        util = TechDraUtility()
+        cureStamina()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+        util.playBGM("lobby")
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        util.stopBGM()
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,5 +63,17 @@ class LobbyViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func cureStamina() {
+        staminaTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "updateStaminaValue", userInfo: nil, repeats: true)
+        staminaTimer.fire()
+    }
+    
+    func updateStaminaValue() {
+        if stamina <= 100 {
+            stamina = stamina + 1
+            staminaBar.progress = stamina / 100
+        }
+    }
 
 }
